1c612c16db37edc5e270857315ae5547ff2de0cc
ElenaStah <150523060+ElenaStah@users.noreply.github.com>
2024-06-08 20:18:45 +0300

    Update lesson_5.task_8.py

--- lesson_5/lesson_5.task_8.py
+++ lesson_5/lesson_5.task_8.py
@@ -8,23 +8,28 @@ driver = webdriver.Chrome()
 
 #открыть ccылку
 driver.get("http://the-internet.herokuapp.com/login")
-driver.find_element(By.CSS_SELECTOR, 'input#username').send_keys("elenastah")
+driver.find_element(By.CSS_SELECTOR, 'input#username').send_keys("tomsmith")
 driver.find_element(By.CSS_SELECTOR, 'input#password').send_keys("SuperSecretPassword!")
 driver.find_element(By.CSS_SELECTOR, 'i.fa').click()
-click.send_keys("1000")
+
 sleep(2)	
 driver.quit()
 
 ## открыть страницу в FireFox
-# from selenium import webdriver
-# from webdriver_manager.firefox import GeckoDriverManager().install())
-# driver = webdriver.Firefox(executable_path = GeckoDriverManager().install())
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+driver = webdriver.Firefox(service=FirefoxService(
+GeckoDriverManager().install()))
 
 #открыть ccылку
 driver.get("http://the-internet.herokuapp.com/login")
-driver.find_element(By.CSS_SELECTOR, 'input#username').send_keys("elenastah")
+driver.find_element(By.CSS_SELECTOR, 'input#username').send_keys("tomsmith")
 driver.find_element(By.CSS_SELECTOR, 'input#password').send_keys("SuperSecretPassword!")
 driver.find_element(By.CSS_SELECTOR, 'i.fa').click()
-click.send_keys("1000")
+
 sleep(2)	
 driver.quit()
