c04f13114a05ba70759eb2e3023bbe228097fbb7
ElenaStah <150523060+ElenaStah@users.noreply.github.com>
2024-06-08 20:14:41 +0300

    Update lesson_5.task_6.py

--- lesson_5/lesson_5.task_6.py
+++ lesson_5/lesson_5.task_6.py
@@ -11,21 +11,29 @@ driver.get("http://the-internet.herokuapp.com/entry_ad")
 sleep(5)
 
 #кликнуть на кнопку 
-button = driver.find_element(By.CSS_SELECTOR, "div.modal - footer p").click()
+button = driver.find_element(By.CSS_SELECTOR, "div.modal-footer p").click()
        
 sleep(5)	
 driver.quit()
 
+
 ## открыть страницу в FireFox
-# from selenium import webdriver
-# from webdriver_manager.firefox import GeckoDriverManager().install())
-# driver = webdriver.Firefox(executable_path = GeckoDriverManager().install())
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+driver = webdriver.Firefox(service=FirefoxService(
+GeckoDriverManager().install()))
+
 #открыть сайт
 driver.get("http://the-internet.herokuapp.com/entry_ad")
 sleep(5)
 
 #кликнуть на кнопку 
-button = driver.find_element(By.CSS_SELECTOR, "div.modal - footer p").click()
+button = driver.find_element(By.CSS_SELECTOR, "div.modal-footer p").click()
        
 sleep(5)	
 driver.quit()
+
