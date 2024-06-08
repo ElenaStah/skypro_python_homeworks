f3da2c9c20f93fa2cb89d47af5e20b4e477f15ef
ElenaStah <150523060+ElenaStah@users.noreply.github.com>
2024-06-08 22:53:39 +0300

    Update lesson_6_task_1.py

--- lesson_6/lesson_6_task_1.py
+++ lesson_6/lesson_6_task_1.py
@@ -1,16 +1,19 @@
 from selenium import webdriver
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
-from time import sleep
-from selenium.webdriver.common.by import By
 
-driver = webdriver.Chrome()
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 wait = WebDriverWait(driver, 40, 0.1)
 
-driver.get('https://uitestinqplayground.com/ajax')
+driver.get('http://uitestingplayground.com/ajax')
 driver.find_element(By.CSS_SELECTOR, "#ajaxButton").click()
 
-text = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, ".bg-success"))).text
-
-sleep(2)
-print(text)
+# Дождаться появления текста в зеленой плашке и затем продолжить
+try:green_box_text = WebDriverWait(driver, 16).until(EC.visibility_of_element_located((By.CSS_SELECTOR, "p.bg-success"))).text
+except TimeoutException:
+    print("Элемент не был найден в отведенное время.")
+print(green_box_text) 
