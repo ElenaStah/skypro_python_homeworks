cd08bfc3871b8de216ee30afc1308dd252a38c6a
ElenaStah <150523060+ElenaStah@users.noreply.github.com>
2024-06-08 20:11:05 +0300

    Update lesson_5.task_4.py

--- lesson_5/lesson_5.task_4.py
+++ lesson_5/lesson_5.task_4.py
@@ -4,38 +4,36 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+
 driver = webdriver.Chrome()
 #открыть сайт
 driver.get("http://uitestingplayground.com/dynamicid")
 
 #кликнуть на кнопку "Add element" 3 раза
-
-Add_button = "button.btn-primary"
-click = driver.find_element(By.CSS_SELECTOR, Add_button)
 for n in range(3):
-       click.click()
-       a = driver.switch_to.alept.accept()
+       driver.find_element(By.CSS_SELECTOR, "button.btn-primary").click()
        sleep(1)
-sleep(5)
-	
+
+sleep(5)	
 driver.quit()
 
 ## открыть страницу в FireFox
-# from selenium import webdriver
-# from webdriver_manager.firefox import GeckoDriverManager().install())
-# driver = webdriver.Firefox(executable_path = GeckoDriverManager().install())
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+driver = webdriver.Firefox(service=FirefoxService(
+GeckoDriverManager().install()))
 
 #открыть сайт
 driver.get("http://uitestingplayground.com/dynamicid")
 
 #кликнуть на кнопку "Add element" 3 раза
-
-Add_button = "button.btn-primary"
-click = driver.find_element(By.CSS_SELECTOR, Add_button)
 for n in range(3):
-       click.click()
-       a = driver.switch_to.alept.accept()
+       driver.find_element(By.CSS_SELECTOR, "button.btn-primary").click()
        sleep(1)
-sleep(5)
-	
-driver.quit() 	
+
+sleep(5)	
+driver.quit()
