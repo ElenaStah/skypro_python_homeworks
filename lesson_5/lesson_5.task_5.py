cce40a9cde8b148c38d278ec58357856233b8f19
ElenaStah <150523060+ElenaStah@users.noreply.github.com>
2024-06-08 20:13:10 +0300

    Update lesson_5.task_5.py

--- lesson_5/lesson_5.task_5.py
+++ lesson_5/lesson_5.task_5.py
@@ -1,39 +1,44 @@
-from time import sleep
+ 	from time import sleep
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 driver = webdriver.Chrome()
-#открыть сайт
-driver.get("http://uitestingplayground.com/classattr")
 
-#кликнуть на кнопку "Add element" 
-Add_button = "button.btn-primary"
-click = driver.find_element(By.CSS_SELECTOR, Add_button)
+#открыть сайт
+driver.get("http://uitestingplayground.com/classattr") 
+Add_Element = "button.btn-primary"
+click = driver.find_element(By.CSS_SELECTOR, Add_Element)
+
+#Трижды кликнуть по синей кнопке
 for n in range(3):
        click.click()
-       a = driver.switch_to.alept.accept()
+       a = driver.switch_to.alert.accept()
        sleep(1)
-sleep(5)
-	
+sleep(5)	
 driver.quit()
 
+
 ## открыть страницу в FireFox
-# from selenium import webdriver
-# from webdriver_manager.firefox import GeckoDriverManager().install())
-# driver = webdriver.Firefox(executable_path = GeckoDriverManager().install())
-##Кликнуть по синей кнопке
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+driver = webdriver.Firefox(service=FirefoxService(
+GeckoDriverManager().install()))
+
 #открыть сайт
 driver.get("http://uitestingplayground.com/classattr")
+Add_Element = "button.btn-primary"
+click = driver.find_element(By.CSS_SELECTOR, Add_Element)
 
-#кликнуть на кнопку "Add element" 
-Add_button = "button.btn-primary"
-click = driver.find_element(By.CSS_SELECTOR, Add_button)
+#Трижды кликнуть по синей кнопке
 for n in range(3):
        click.click()
-       a = driver.switch_to.alept.accept()
+       a = driver.switch_to.alert.accept()
        sleep(1)
-sleep(5)
-	
+sleep(5)	
 driver.quit() 	
