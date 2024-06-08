a9073d20b16ecc4c4867d6d584c507e5d2628716
ElenaStah <150523060+ElenaStah@users.noreply.github.com>
2024-06-08 20:17:09 +0300

    Update lesson_5.task_7.py

--- lesson_5/lesson_5.task_7.py
+++ lesson_5/lesson_5.task_7.py
@@ -19,9 +19,14 @@ sleep(2)
 driver.quit()
 
 ## открыть страницу в FireFox
-# from selenium import webdriver
-# from webdriver_manager.firefox import GeckoDriverManager().install())
-# driver = webdriver.Firefox(executable_path = GeckoDriverManager().install())
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+driver = webdriver.Firefox(service=FirefoxService(
+GeckoDriverManager().install()))
 
 #открыть ccылку
 driver.get("http://the-internet.herokuapp.com/inputs")
